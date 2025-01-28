e1033a9e5306d8f1865e17259046bb59c6803fa7
oak <dubodeldanila@gmail.com>
2025-01-28 23:26:55 +0300

    change layout styles

--- src/components/common/layout.tsx
+++ src/components/common/layout.tsx
@@ -1,13 +1,12 @@
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-import { AppBar, Box, Button, Container, IconButton, Toolbar, styled } from '@mui/material';
+import { AppBar, Box, Button, Grid2 as Grid, IconButton, Toolbar, styled } from '@mui/material';
 import { useNavigate } from '@tanstack/react-location';
 import { SnackBar } from 'components/common/utility/snackbar';
 import { HideOnScroll } from 'components/login/scroll';
 import { ROUTES } from 'constants/routes';
 import logo from 'img/tex-text.png';
 import { FC, ReactNode } from 'react';
-
 interface LayoutProps {
   children: ReactNode;
 }
@@ -32,35 +31,37 @@ export const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-      <HideOnScroll>
-        <AppBar
-          position='fixed'
-          sx={{
-            backgroundColor: 'transparent',
-            boxShadow: 'none',
-          }}
-        >
-          <PrintHiddenToolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button
-              variant='contained'
-              onClick={() => window.history.back()}
-              sx={{ whiteSpace: 'nowrap' }}
-              size='small'
-            >
-              <ArrowBackIosIcon fontSize='small' />
-            </Button>
-            <IconButton onClick={navigateMainPage}>
-              <img src={logo} style={{ width: '30px', height: '30px' }} />
-            </IconButton>
-            <Button variant='outlined' color='secondary' size='small' onClick={handleLogout}>
-              <ExitToAppIcon fontSize='small' />
-            </Button>
-          </PrintHiddenToolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Container component='main' sx={{ paddingTop: '64px' }}>
-        {children}
-      </Container>
+      <Box sx={{ margin: '10px 20px 64px 20px' }}>
+        <HideOnScroll>
+          <AppBar
+            position='fixed'
+            sx={{
+              backgroundColor: 'transparent',
+              boxShadow: 'none',
+            }}
+          >
+            <PrintHiddenToolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                variant='contained'
+                onClick={() => window.history.back()}
+                sx={{ whiteSpace: 'nowrap' }}
+                size='small'
+              >
+                <ArrowBackIosIcon fontSize='small' />
+              </Button>
+              <IconButton onClick={navigateMainPage}>
+                <img src={logo} style={{ width: '30px', height: '30px' }} />
+              </IconButton>
+              <Button variant='outlined' color='secondary' size='small' onClick={handleLogout}>
+                <ExitToAppIcon fontSize='small' />
+              </Button>
+            </PrintHiddenToolbar>
+          </AppBar>
+        </HideOnScroll>
+      </Box>
+      <Grid container px={2} display='flex' justifyContent='center'>
+        <Grid>{children}</Grid>
+      </Grid>
       <SnackBar />
     </Box>
   );
