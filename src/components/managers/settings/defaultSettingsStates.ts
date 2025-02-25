97b33472be6c35294e3524c08b88fe12f0c98cd8
oak <dubodeldanila@gmail.com>
2025-02-25 15:45:09 +0300

    refactor settings manager

--- src/components/managers/settings/defaultSettingsStates.ts
+++
@@ -1,8 +0,0 @@
-import { UpdateSettingsRequest } from "api/proto-http/admin"
-
-export const defaultSettingsStates: UpdateSettingsRequest = {
-    shipmentCarriers: undefined,
-    paymentMethods: undefined,
-    siteAvailable: undefined,
-    maxOrderItems: undefined
-}
--- src/ui/components/checkbox.tsx
+++ src/ui/components/checkbox.tsx
@@ -1,11 +1,20 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
 
-export default function CheckboxCommon({ name, ...props }: { name: string; [k: string]: unknown }) {
+export default function CheckboxCommon({
+  name,
+  onChange,
+  ...props
+}: {
+  name: string;
+  onChange?: (checked: boolean) => void;
+  [k: string]: unknown;
+}) {
   return (
     <Checkbox.Root
       className='flex h-3 w-3 flex-none appearance-none items-center justify-center border border-text'
       id={name}
       name={name}
+      onCheckedChange={onChange}
       {...props}
     >
       <Checkbox.Indicator className='h-full w-full bg-text'></Checkbox.Indicator>
