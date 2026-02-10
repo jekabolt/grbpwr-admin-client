4e62cdb00234bb0eb56ae50c443e1ad91ae3f9c2
0101oak <115372729+0101oak@users.noreply.github.com>
2026-02-10 10:15:20 +0000

    Deploying to gh-pages from @ jekabolt/grbpwr-admin-client@b62180f6c952177785aca52ee82fb812a0f317f3 🚀

--- assets/page-C8VW2X6-.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./mui--1QQx6Xz.js";import"./vendor-BqCw_tH9.js";function n(){return i.jsx("div",{children:"Analitic"})}export{n as Analitic};
---
+++ assets/page-knhNASGY.js
@@ -0,0 +1,1 @@
+import{j as i}from"./mui-D9Bk7yoK.js";import"./vendor-DljJgkTU.js";function n(){return i.jsx("div",{children:"Analitic"})}export{n as Analitic};
